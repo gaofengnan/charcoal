d7b25dd6f1eb6150c808ff8f813496f17acc1028
gaofengnan <gao.fengnan@gmail.com>
2022-08-15 20:39:10 +0800

    minor update to 0.13

--- R/charcoal-package.R
+++ R/charcoal-package.R
@@ -1,12 +1,13 @@
 #' charcoal: A package for changepoint localization in high-dimensional linear 
 #' regression
-#' The charcoal package collects the implementation for the novel methodology
-#' 'charcoal' for changepoint localization in high-dimensional linear
-#' regression, introduced in Gao and Wang (2022).
+#' 
 #'
 #' @docType package
 #' @name charcoal
 #'
+#' @description The charcoal package collects the implementation for the novel methodology
+#' 'charcoal' for changepoint localization in high-dimensional linear
+#' regression, introduced in Gao and Wang (2022).
 #' @importFrom evd fgev qgev
 #' @importFrom RSpectra svds
 #' @importFrom MASS mvrnorm
